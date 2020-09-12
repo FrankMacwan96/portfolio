3849ba7c3519d1d81b8c2791967a21bd663fc70e
Frank <macwanfrank96@gmail.com>
2020-09-12 19:50:17 -0400

    First Commit

---
+++ src/components/hero/hero.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Row, Col } from "react-flexbox-grid";
+// SCSS
+import "./hero.scss";
+//Assets
+import HeroImage from '../../assets/hero/hero-image.png';
+//Components
+import Button from '../ui-components/button/button';
+
+const hero = () => (
+  <div className="hero" id="hero">
+    <div className="wrapper">
+      <Row>
+        <Col md={12} lg={6}>
+          <div className="hero-info">
+            <h1 className="weight800 font60">Frank Macwan</h1>
+            <h3 className="weight800 font40">
+              Full Stack / Software Developer
+            </h3>
+            <p className="font12">
+              I am passionate developer with keen interest and skills in both front end and back end development.
+            </p>
+            <Button label="SEND MESSAGE" target={"contact"} />
+          </div>
+        </Col>
+        <Col md={12} lg={6}>
+          <div className="hero-image">
+            <img src={HeroImage} alt="hero" />
+          </div>
+        </Col>
+      </Row>
+    </div>
+  </div>
+);
+
+export default hero;
